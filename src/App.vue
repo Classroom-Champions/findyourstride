88bb29268117fb820946821ae7f5b19680234c76
Brandon H <12652355+bhalvy@users.noreply.github.com>
2019-12-07 17:46:38 -0600

    update .gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,16 @@
 <template>
   <div id="app">
     <Splash />
-    <Manifesto />
-    <SocialProof />
-    <TeaserContent />
-    <Testimonial />
-    <Cta />
-    <About />
   </div>
 </template>
 
 <script>
-import About from './components/about';
-import Cta from './components/cta';
-import Manifesto from './components/manifesto';
-import SocialProof from './components/socialProof';
-import Splash from './components/splash';
-import TeaserContent from './components/teaserContent';
-import Testimonial from './components/testimonial';
+
 
 export default {
   name: 'app',
   components: {
-    TeaserContent,
-    Splash,
-    SocialProof,
-    Manifesto,
-    Testimonial,
-    Cta,
-    About,
+
   }
 }
 </script>
