e7fc2d13c1d5a76a50e2ec8d4d5ad4b16cbb421e
Jimmy Hong <jimmyhong.rocks@gmail.com>
2019-12-07 20:41:12 -0600

    sample button styles

---
+++ src/components/shared/button.vue
@@ -0,0 +1,37 @@
+<template>
+  <component :is="type" :href="href" class="button">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: null
+    },
+    to: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    type() {
+      if (this.href) {
+        return "a";
+      } else {
+        return "button";
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.button {
+  background-color: #59bcea;
+  border: none;
+  border-radius: 20px;
+}
+</style>
