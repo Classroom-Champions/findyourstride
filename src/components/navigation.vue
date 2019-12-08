6a51cdeedf2f721f1d18d84658153ecfbe668031
Geoff Hensley <geoffhensley@gmail.com>
2019-12-07 19:25:03 -0600

    Shape expansion on click

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,16 +1,25 @@
+<style scoped>
+
+</style>
 <template>
 <div>
     <H1>Navigation</H1>
-    <img class="shape" :src="require('@/assets/triangle.svg')">
+    <img class="shape" :src="require('@/assets/triangle.svg')" @click="makeSelection" :class="{ 'shape-full': isSelected }">
 </div>
 </template>
 
 <script>
-    export default {
-        name: "Navigation"
-    }
+export default {
+    name: "Navigation",
+    data() {
+      return {
+        isSelected: false
+      };
+    },
+    methods: {
+        makeSelection() {
+            this.isSelected = true;
+        }
+    },
+}
 </script>
-
-<style scoped>
-
-</style>
