fc79cf081513392525ea43930053cebae79c2d0b
Geoff Hensley <geoffhensley@gmail.com>
2019-12-07 21:03:02 -0600

    Full scale shapes

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -2,10 +2,31 @@
 
 </style>
 <template>
-<div>
-    <H1>Navigation</H1>
-    <img class="shape" :src="require('@/assets/triangle.svg')" @click="makeSelection" :class="{ 'shape-full': isSelected }">
-</div>
+    <div>
+        <div @click="makeSelection" :class="{ 'shape--size-full': isSelected }">
+            <img class="shape" :src="require('@/assets/triangle.svg')">
+
+            <div class="shape-content">
+                <div class="container">
+                    <div class="row">
+                        <div class="one column">One</div>
+                        <div class="eleven columns">Eleven</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div @click="makeSelection" :class="{ 'shape--size-full': isSelected }">
+            <img class="shape" :src="require('@/assets/triangle.svg')">
+
+            <div class="shape-content">
+                <div class="container">
+                    <div class="row">
+                        here we goooo
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -13,7 +34,7 @@ export default {
     name: "Navigation",
     data() {
       return {
-        isSelected: false
+        isSelected: false,
       };
     },
     methods: {
